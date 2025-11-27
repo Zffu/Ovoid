6b5a38f6e2e308c8183f68765dc800d1cb55bdfe
Zffu <103074097+Zffu@users.noreply.github.com>
2025-11-27 21:40:24 +0100

    feat: added data_token base

--- lexer/includes/token.hpp
+++ lexer/includes/token.hpp
@@ -2,16 +2,35 @@
 
 namespace ovoid::lexer {
 
+/**
+ * @brief A type of lexer token.
+ */
 enum token_type {
 
 };
 
+/**
+ * @brief A lexer token
+ */
 class token {
 public:
 	token_type type;
 
 	token(token_type type) {
 		this->type = type;
+	}
+};
+
+/**
+ * @brief A token containing data.
+ */
+template <typename T> 
+class data_token: public token {
+public:
+	T data;
+
+	data_token(token_type type, T data): token(type) {
+		this->data = data;
 	}
 };
 
