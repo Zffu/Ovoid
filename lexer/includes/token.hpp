8c99d322e3293e25d0f314a6d3397c678db6213f
Zffu <103074097+Zffu@users.noreply.github.com>
2025-11-27 21:46:25 +0100

    feat: added sample types

--- lexer/includes/token.hpp
+++ lexer/includes/token.hpp
@@ -1,12 +1,18 @@
 #pragma once
 
+#include <string>
+
 namespace ovoid::lexer {
 
 /**
  * @brief A type of lexer token.
  */
 enum token_type {
+	KEYWORD = 0x00,
+	STRING = 0x01,
 
+	BOOLEAN = 0x02,
+	INTEGER = 0x03
 };
 
 /**
@@ -32,6 +38,36 @@ public:
 	data_token(token_type type, T data): token(type) {
 		this->data = data;
 	}
+};
+
+/**
+ * Sample token types
+ */
+
+/**
+* @brief A keyword token
+*/
+class keyword_token: public data_token<std::string> {
+public:
+	keyword_token(std::string tok): data_token(token_type::KEYWORD, tok) {}
+};
+
+/**
+ * @brief A string token.
+ */
+class string_token: public data_token<std::string> {
+public:
+	string_token(std::string str): data_token(token_type::STRING, str) {}
+};
+
+class bool_token: public data_token<bool> {
+public:
+	bool_token(bool val): data_token(token_type::BOOLEAN, val) {}
+};
+
+class int_token: public data_token<int> {
+public:
+	int_token(int val): data_token(token_type::INTEGER, val) {}
 };
 
 }
